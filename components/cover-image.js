6f7b429aba8c01457947afd391ad9e7c3aa43bec
Fábio Oliveira <foliveira@users.noreply.github.com>
2023-04-15 11:08:09 +0100

    Update cover-image.js

--- components/cover-image.js
+++ components/cover-image.js
@@ -4,18 +4,17 @@ import Image from 'next/image'
 
 export default function CoverImage({ title, src, slug }) {
   const image = (
-    <div style={{width: '100%', height: '100%', position: 'relative'}}>
       <Image
         src={src}
         fill
         objectFit="contain"
+        sizes="100vw"
         placeholder="blur"
         alt={`Cover Image for ${title}`}
         className={cn('shadow-small', {
           'hover:shadow-medium transition-shadow duration-200': slug,
         })}
       />
-    </div>
   )
   return (
     <div className="sm:mx-0">
