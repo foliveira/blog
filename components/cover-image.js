43f3d8f38538914ed389b2c06cef23f167d66911
Fabio Oliveira <fabio.an.oliveira@gmail.com>
2023-04-15 11:04:05 +0100

    Changes to image sizing behaviour

--- components/cover-image.js
+++ components/cover-image.js
@@ -4,15 +4,18 @@ import Image from 'next/image'
 
 export default function CoverImage({ title, src, slug }) {
   const image = (
-    <Image
-      src={src}
-      placeholder="blur"
-      width="100vw"
-      alt={`Cover Image for ${title}`}
-      className={cn('shadow-small', {
-        'hover:shadow-medium transition-shadow duration-200': slug,
-      })}
-    />
+    <div style={{width: '100%', height: '100%', position: 'relative'}}>
+      <Image
+        src={src}
+        fill
+        objectFit="contain"
+        placeholder="blur"
+        alt={`Cover Image for ${title}`}
+        className={cn('shadow-small', {
+          'hover:shadow-medium transition-shadow duration-200': slug,
+        })}
+      />
+    </div>
   )
   return (
     <div className="sm:mx-0">
