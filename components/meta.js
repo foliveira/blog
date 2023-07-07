491248a4216a033e419f55b5a28d6455d263bafd
Fábio Oliveira <foliveira@users.noreply.github.com>
2023-07-07 09:24:38 +0100

    Update meta.js
    Add Mastodon site verification tag

--- components/meta.js
+++ components/meta.js
@@ -37,6 +37,7 @@ export default function Meta() {
         content={`${BLOG_TITLE}.`}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      <link rel="me" href="https://mastodon.social/@foliveira" />
     </Head>
   )
 }
